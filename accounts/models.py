37537ea6c3c1a5ef2f5e4983d257325959b6a7be
Hebert <hebertjulio@users.noreply.github.com>
2020-05-18 14:43:57 -0300

    ajusta models e cria migrations

--- accounts/models.py
+++ accounts/models.py
@@ -6,20 +6,23 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 
 from model_utils.models import TimeStampedModel
+from model_utils import Choices
 
 from .managers import CustomUserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
 
-    name = models.CharField(_('name'), max_length=50)
     email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(_('name'), max_length=50)
+
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
         help_text=_(
             'Designates whether the user can log into this admin site.'),
     )
+
     is_active = models.BooleanField(
         _('active status'),
         default=True,
@@ -57,3 +60,32 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
+
+
+class Profile(TimeStampedModel):
+
+    ROLE = Choices(
+        ('owner', _('owner')),
+        ('guest', _('guest')),
+    )
+
+    name = models.CharField(_('name'), max_length=30)
+    pin = models.CharField(_('pin'), max_length=4)
+    accountable = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    role = models.CharField(
+        _('role'), max_length=30, db_index=True, choices=ROLE
+    )
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+        unique_together = [
+            ['accountable', 'pin']
+        ]
--- cash_book/models.py
+++ cash_book/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+from model_utils import Choices
+
+
+class Transaction(TimeStampedModel):
+
+    STATUS = Choices(
+        ('pending', _('pending')),
+        ('done', _('done')),
+    )
+
+    description = models.TextField(('description'), blank=True)
+    value = models.DecimalField(_('value'), max_digits=10,  decimal_places=2)
+
+    debtor = models.ForeignKey(
+        'accounts.User', related_name='debtor', on_delete=models.CASCADE
+    )
+
+    creditor = models.ForeignKey(
+        'accounts.User', related_name='creditor', on_delete=models.CASCADE
+    )
+
+    status = models.CharField(
+        _('status'), max_length=30, db_index=True, choices=STATUS
+    )
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = _('transaction')
+        verbose_name_plural = _('transactions')
