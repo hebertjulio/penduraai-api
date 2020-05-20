fb98f17f0efa139bead642529bb34be7c42068a1
Hebert <hebertjulio@users.noreply.github.com>
2020-05-20 16:03:21 -0300

    Atribui debtor ao salvar o objeto para que esse campo seja exibido apenas para leitura

--- cashbooks/serializers.py
+++ cashbooks/serializers.py
@@ -10,9 +10,14 @@ class TransactionSerializer(serializers.ModelSerializer):
     requester = ProfileField()
     signature = ProfileField()
 
-    debtor = serializers.HiddenField(
-        default=serializers.CurrentUserDefault())
+    def create(self, validated_data):
+        request = self.context['request']
+        obj = Transaction(**validated_data)
+        obj.debtor = request.user
+        obj.save()
+        return obj
 
     class Meta:
         model = Transaction
         fields = '__all__'
+        read_only_fields = ('debtor',)
