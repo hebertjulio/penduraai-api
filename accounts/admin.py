fadc396e6686efaafbc55fae674a85b00c2c038b
Hebert <hebertjulio@users.noreply.github.com>
2020-05-20 13:36:59 -0300

    Ajustes nas Admins dos apps accounts e cashbooks

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User, Profile
+from .models import User, Profile, Whitelist
 
 
 @admin.register(User)
@@ -43,9 +43,31 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = (
         'accountable__name', 'accountable__email', 'name',
     )
+    autocomplete_fields = (
+        'accountable',
+    )
     list_filter = (
         'role',
     )
     ordering = (
-        'accountable__name',
+        'accountable__id', 'name',
+    )
+
+
+@admin.register(Whitelist)
+class WhitelistAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'guest', 'status',
+    )
+    search_fields = (
+        'user__name', 'user__email', 'guest__name', 'guest__email',
+    )
+    autocomplete_fields = (
+        'user', 'guest',
+    )
+    list_filter = (
+        'status',
+    )
+    ordering = (
+        'user__id', 'guest__name',
     )
--- cashbooks/admin.py
+++ cashbooks/admin.py
@@ -6,15 +6,18 @@ from .models import Transaction
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = (
-        'creditor', 'debtor', 'description', 'type',
-        'value', 'status',
+        'creditor', 'debtor', 'description', 'operation',
+        'value',
     )
     search_fields = (
         'creditor__name', 'creditor__email', 'debtor__name',
         'debtor__email', 'description',
     )
+    autocomplete_fields = (
+        'creditor', 'debtor', 'requester', 'signature',
+    )
     list_filter = (
-        'status',
+        'operation',
     )
     ordering = (
         'created',
