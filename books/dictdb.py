ab5424f20bc2ced660b6e966fef9769aa0e9fca4
Hebert <hebertjulio@@users.noreply.github.com>
2020-05-24 16:38:10 -0300

    Refatoração do dictdb

--- books/dictdb.py
+++ books/dictdb.py
@@ -8,35 +8,38 @@ class Storage(ABC):
 
     PREFIX = 'storage'
 
-    def __init__(self, key):
+    def __init__(self, id):
         host = settings.DICTDB_REDIS_HOST
         port = settings.DICTDB_REDIS_PORT
         db = settings.DICTDB_REDIS_DB
         self.db = Redis(host=host, port=port, db=db)
-        self.key = key
+        self.id = id
 
     def __setitem__(self, key, value):
-        self.db.set(
-            ':'.join([self.PREFIX, self.key, key]), value
-        )
+        name = ':'.join([self.PREFIX, self.id, key])
+        self.db.set(name, value)
 
     def __getitem__(self, key):
-        return self.db.get(
-            ':'.join([self.PREFIX, self.key, key])
-        )
+        name = ':'.join([self.PREFIX, self.id, key])
+        value = self.db.get(name)
+        return value
 
     def __delitem__(self, key):
-        self.db.delete(
-            ':'.join([self.PREFIX, self.key, key])
-        )
+        name = ':'.join([self.PREFIX, self.id, key])
+        self.db.delete(name)
 
     def __len__(self):
-        return len(self.key())
+        name = ':'.join([self.PREFIX, self.id, '*'])
+        names = self.db.keys(name)
+        count = len(names)
+        return count
+
+    def __contains__(self, key):
+        names = ':'.join([self.PREFIX, self.id, key])
+        count = self.db.exists(*names)
+        return count == 1
 
     def __delattr__(self, instance):
-        self.db.delete(*self.keys())
-
-    def keys(self):
-        return self.db.keys(
-            ':'.join([self.PREFIX, self.key, '*'])
-        )
+        patterns = ':'.join([self.PREFIX, self.id, '*'])
+        names = self.db.keys(patterns)
+        self.db.delete(*names)
