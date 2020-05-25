2e693d3b33669f110e86343aed7fe36e6c969971
Hebert <hebertjulio@@users.noreply.github.com>
2020-05-25 10:36:54 -0300

    Cria função para fazer a tradução dos nomes dos campos das chaves estrangeiras

--- broker/serializers.py
+++ broker/serializers.py
@@ -7,6 +7,7 @@ from model_utils import Choices
 from books.serializers import RecordCreateSerializer
 
 from .storages import Transaction
+from .services import translate_fk_names
 
 
 class TransactionSerializer(serializers.Serializer):
@@ -20,23 +21,15 @@ class TransactionSerializer(serializers.Serializer):
     data = serializers.JSONField(write_only=True)
     channel_name = serializers.CharField(max_length=255, write_only=True)
 
-    def validate_data(self, value):
-        # translate fields
-        field = {
-            'creditor': 'creditor_id', 'debtor': 'debtor_id',
-            'buyer': 'buyer_id', 'seller': 'seller_id',
-        }
-        value = {
-            **{field[k]: v for k, v in value.items() if k in field.keys()},
-            **{k: v for k, v in value.items() if k not in field.keys()}
-        }
-        return value
-
     def create(self, validated_data):
         transaction = str(uuid.uuid4())
         tran = Transaction(transaction)
         tran.channel_name = validated_data['channel_name']
-        tran.data = json.dumps(validated_data['data'])
+        data = translate_fk_names(validated_data['data'])
+        tran.data = json.dumps(data)
         return {
             'transaction': transaction
         }
+
+    def update(self, instance, validated_data):
+        pass
