6ad4ce362e1560081488adff8196dc1695388868
Hebert <hebertjulio@@users.noreply.github.com>
2020-05-27 14:31:49 -0300

    Unnecessary comprehension -  can take a generator

--- broker/services.py
+++ broker/services.py
@@ -5,7 +5,7 @@ def generate_signature(transaction, data):
     """generate signature to integrity check"""
     if not isinstance(data, dict):
         raise ValueError
-    value = ';'.join([v for _, v in sorted(data.items())])
+    value = ';'.join(v for _, v in sorted(data.items()))
     value = transaction + ':' + value
     h = hashlib.sha256(value.encode())
     return h.hexdigest()
