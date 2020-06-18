3b756166ef32868a7b3d0ca704bc60b37d95ae78
Hebert <hebertjulio@@users.noreply.github.com>
2020-06-17 21:43:20 -0300

    An attribute defined in json.encoder line 158 hides this method

--- bridges/encoders.py
+++ bridges/encoders.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 
 class DecimalEncoder(JSONEncoder):
 
-    def default(self, o):  # noqa
+    def default(self, o):  # skipcq
         if isinstance(o, Decimal):
             return float(o)
         return super().default(o)
