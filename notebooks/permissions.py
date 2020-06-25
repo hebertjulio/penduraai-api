0a5ce42037459962468a92cb0d75e66c323f48d1
Hebert <hebertjulio@@users.noreply.github.com>
2020-06-25 20:10:39 -0300

    Recupera loja na transação para validar se cliente pode comprar nela

--- notebooks/permissions.py
+++ notebooks/permissions.py
@@ -6,17 +6,26 @@ from .models import Record
 
 class CanBuy(IsOwner):
 
+    @classmethod
+    def get_store(cls, view):
+        if hasattr(view, 'transaction'):
+            obj = view.transaction
+            data = obj.get_data()
+            if 'store' in data.keys():
+                return data['store']
+        return None
+
     def has_permission(self, request, view):
         has_permission = super().has_permission(request, view)
-        profile = request.user.profile
-        if profile and not has_permission:
+        if not has_permission:
             # Ignore buy permission if payment
             if 'operation' in request.data:
                 operation = request.data['operation']
                 if operation == Record.OPERATION.credit:
                     return True
-            if 'store' in request.data:
-                store = request.data['store']
+            store = self.get_store(view)
+            if store is not None:
+                profile = request.user.profile
                 qs = profile.profilebuyers.select_related('sheet')
                 qs = qs.filter(sheet__store=store)
                 return qs.exists()
