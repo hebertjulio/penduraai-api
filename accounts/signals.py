881f78073d2a33b816ecf5d216404c521c46bf71
Hebert <hebertjulio@users.noreplay.github.com>
2020-07-04 10:31:42 -0300

    Cria profile owner do super usuário após sua criação.

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -5,15 +7,18 @@ from .models import User, Profile
 
 
 @receiver(post_save, sender=User)
-def owner_profile_name_update(sender, instance, created, **kwargs):
-    """ Change owner profile name when user name is changed """
-    if created:
-        return
-    try:
-        profile = instance.userprofiles.get(role=Profile.ROLE.owner)
-        if profile.name == instance.name:
-            return
-        profile.name = instance.name
-        profile.save()
-    except Profile.DoesNotExist:
-        pass
+def owner_profile(sender, instance, created, **kwargs):
+    if not created:
+        try:
+            obj = instance.userprofiles.get(role=Profile.ROLE.owner)
+            if obj.name != instance.name:
+                obj.name = instance.name
+                obj.save()
+        except Profile.DoesNotExist:
+            pass
+    else:
+        pin = randint(1000, 9999)  # nosec
+        obj = Profile(**{
+            'name': instance.name, 'pin': pin,
+            'role': Profile.ROLE.owner, 'user': instance})
+        obj.save()
