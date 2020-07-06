56307e89b592c0b254a5561790c8818a97db8ae8
Hebert <hebertjulio@users.noreplay.github.com>
2020-07-06 10:46:04 -0300

    Remove criação de profile na criação de superusuário

--- accounts/signals.py
+++ accounts/signals.py
@@ -11,14 +11,9 @@ def owner_profile(sender, instance, created, **kwargs):
     if not created:
         try:
             obj = instance.userprofiles.get(role=Profile.ROLE.owner)
+        except Profile.DoesNotExist:
+            pass
+        else:
             if obj.name != instance.name:
                 obj.name = instance.name
                 obj.save()
-        except Profile.DoesNotExist:
-            pass
-    else:
-        pin = randint(1000, 9999)  # nosec
-        obj = Profile(**{
-            'name': instance.name, 'pin': pin,
-            'role': Profile.ROLE.owner, 'user': instance})
-        obj.save()
