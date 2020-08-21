178ca12945ac238fefdec64c6300c230dcdd2222
Hebert <hebertjulio@users.noreply.github.com>
2020-08-21 14:33:04 -0300

    __init__ method from the base class not called

--- bridges/exceptions.py
+++ bridges/exceptions.py
@@ -10,4 +10,4 @@ class TokenDecodeException(APIException):
     default_code = 'invalid'
 
     def __init__(self, detail):
-        self.detail = _(detail)
+        super().__init__(self, _(detail))
