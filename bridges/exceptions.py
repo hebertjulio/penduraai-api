6b0f471a8933c5c26e04bb1ba6596e5691ff0f3b
Hebert Júlio <hebertjulio@yahoo.com.br>
2020-10-07 11:40:53 -0300

    Remove arquivo desnecessário bridges/exceptions.py

--- bridges/exceptions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.exceptions import APIException
-from rest_framework.status import HTTP_400_BAD_REQUEST
-
-
-class BadRequest(APIException):
-
-    status_code = HTTP_400_BAD_REQUEST
-    default_code = 'invalid'
